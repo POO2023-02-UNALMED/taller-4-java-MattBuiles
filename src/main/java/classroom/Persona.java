aafd035bfdc75a86147e94650c9c7f3d548e68db
MattBuiles <141962207+MattBuiles@users.noreply.github.com>
2023-09-02 12:42:58 -0500

    Modificación 1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,12 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     public Persona(long cedula, String nombre) {
@@ -30,6 +29,12 @@ public class Persona {
     }
 
     public Persona(String nombre) {
+        this.nombre = "";
+        this.cedula=1;
+        totalPersonas++;
+    }
+
+    public Persona(){
         this.nombre = "";
         totalPersonas++;
     }
